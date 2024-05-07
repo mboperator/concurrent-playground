0bfd05edbe5ffe80518b1aa81f5dd9b4f4c844b3
Marcus <marcus@totum.io>
2024-05-07 14:10:24 -0700

    Added more philosophers to see if my solution holds up

--- philosophers.test.ts
+++ philosophers.test.ts
@@ -2,11 +2,13 @@ import {Fork, Philosopher} from "./philosophers";
 
 describe('Dining Philosophers Problem', () => {
   it('should allow philosophers to eat without causing a deadlock', async () => {
-    const forks = [new Fork(), new Fork(), new Fork()];
+    const forks = [new Fork(), new Fork(), new Fork(), new Fork(), new Fork()];
     const philosophers = [
       new Philosopher({ name: 'Philosopher 1', leftFork: forks[0], rightFork: forks[1] }),
       new Philosopher({ name: 'Philosopher 2', leftFork: forks[1], rightFork: forks[2] }),
-      new Philosopher({ name: 'Philosopher 3', leftFork: forks[2], rightFork: forks[0] }),
+      new Philosopher({ name: 'Philosopher 3', leftFork: forks[2], rightFork: forks[3] }),
+      new Philosopher({ name: 'Philosopher 4', leftFork: forks[3], rightFork: forks[4] }),
+      new Philosopher({ name: 'Philosopher 5', leftFork: forks[4], rightFork: forks[0] }),
     ];
 
     const hangingOut = philosophers.map(philosopher => philosopher.hangOutUntilFull());
